550b3c35c31321213cc68c16141aa3a7c9bdd5c9
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2021-10-05 11:28:35 +0900

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,36 @@
+module.exports = (ctx) => ({
+    plugins: {
+        "postcss-import-ext-glob": {},
+        "postcss-import": {},
+        "postcss-mixins": {},
+        "postcss-cssnext": {},
+        "postcss-simple-vars": {
+            silent: true,
+        },
+        "postcss-nested": {},
+        "postcss-mixins": {},
+        "postcss-extend": {},
+        "postcss-color-function": {},
+        'postcss-sorting': {
+            'order': [
+                'custom-properties',
+                'dollar-variables',
+                'declarations',
+                'at-rules',
+                'rules'
+            ],
+            'properties-order': 'alphabetical',
+            'unspecified-properties-position': 'bottom'
+        },
+        "postcss-sort-media-queries": {}, //
+        "cssnano": {
+            "autoprefixer": false,
+            preset: [
+                'default',
+                {
+                    discardComments: { removeAll: false },
+                },
+            ],
+        },
+    }
+});
