620be53216124fcc93bc63e688f79612ef2ee514
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2021-10-20 01:30:25 +0900

    [Add] Added the ability to make the drop-down feature work only on mobile devices.
    When viewed on a PC, the drop-down function will be disabled and the user will be redirected to the link by JS.

--- assets/src/js/accordion.js
+++ assets/src/js/accordion.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
     new Accordion('p-accordions', 'js-accordion-header');
-    new Accordion('p-dropdownMenu', 'js-dropdownMenu-header');
+    new Accordion('s-drawer-dropdown', 'js-drawer-dropdown-header');
+    footerDropdownMenu('index.html');
 });
 
 class Accordion {
@@ -50,100 +51,18 @@ class Accordion {
 }
 
 /**
- * <div class="p-accordion-item">
-    <div class="p-accordion-header js-accordion-header">
-        <button type="button" class="p-accordion-tab">
-            見積は無料ですか？
-            <span class="p-accordion-tabicon"></span>
-        </button>
-    </div>
-    <div class="p-accordion-panel js-accordion-panel">
-        <div class="p-accordion-content">
-            無料です。金額を知りたいだけなどでも、できる限りお答えしております。
-        </div>
-    </div>
-</div>
-
-<style>
-.p-accordion-item {
-    background-color: #ffffff;
-    color: #333333;
-    border: 1px solid #333;
-}
-.p-accordion-item + .p-accordion-item {
-    margin-top: 30px;
-}
-
-.p-accordion-tab {
-    display: flex;
-    align-items: center;
-    height: 68px;
-    font-size: 18px;
-    font-weight: 500;
-    text-align: left;
-    position: relative;
-    padding: 0 68px 0 30px;
-    width: 100%;
-    overflow: hidden;
-}
-
-.p-accordion-tab::before {
-    content: 'Q';
-    display: block;
-    color: #D33429;
-    font-size: 18px;
-    font-weight: bold;
-    width: 1em;
-    line-height: 1;
-    margin-top: -0.2em;
-    margin-right: 15px;
-}
-
-.p-accordion-tabicon {
-    display: inline-block;
-    height: 68px;
-    pointer-events: none;
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 68px;
-    background-color: #49403C;
-}
-
-.p-accordion-tabicon::before,
-.p-accordion-tabicon::after {
-    background-color: #ffffff;
-    bottom: 0;
-    content: "";
-    display: inline-block;
-    height: 2px;
-    left: 0;
-    margin: auto;
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 30px;
-}
-
-.p-accordion-tabicon::after {
-    transform: rotate(90deg);
-    transition: transform 0.3s ease-out 0s;
-}
-
-.p-accordion-header.is-open-panel .p-accordion-tabicon::after {
-    transform: rotate(180deg);
-}
-
-.p-accordion-panel {
-    max-height: 0;
-    overflow: hidden;
-    transition: max-height 0.3s ease-out;
-}
-
-.p-accordion-content {
-    padding: 30px 50px;
-    background-color: #F7E6E1;
-}
-</style>
+ * モバイル端末時: ドロップダウンメニュー, PC端末: location.hrefにて遷移させる
+ * @param {string} destination 遷移先のリンクを文字列で記述
+ * @returns 
  */
 
+function footerDropdownMenu(destination) {
+    const footerDropdown = document.querySelector('.s-footer-dropdown__tab');
+    if (window.matchMedia(`(max-width: 992px)`).matches) {
+        new Accordion('s-footer-dropdown', 'js-footer-dropdown-header');
+    } else {
+        footerDropdown.addEventListener('click', () => {
+            location.href = destination;
+        });
+    }
+}
