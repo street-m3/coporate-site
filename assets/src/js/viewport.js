27452aeca9d2c78d608f4a5523d2bc5e9d1a3b31
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2021-11-02 10:15:53 +0900

    [Add] Add a script for iPhoneSE to support viewport360px display.

---
+++ assets/src/js/viewport.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    switchViewport();
+});
+
+function switchViewport() {
+    const viewport = document.querySelector('meta[name="viewport"]');
+
+    const viewportResize = () => {
+        const width = window.outerWidth > 360 ?
+            'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' :
+            'width=360';
+
+        if (viewport.getAttribute('content') !== width) {
+            viewport.setAttribute('content', width);
+        }
+
+        addEventListener('resize', switchViewport, false);
+        viewportResize();
+    }
+}
