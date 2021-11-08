9d243509dcaf322df04afc6a574531596c851601
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2021-11-08 14:26:59 +0900

    [Change]

--- assets/dist/js/viewport.js
+++ assets/dist/js/viewport.js
@@ -6,7 +6,7 @@ function switchViewport() {
     const viewport = document.querySelector('meta[name="viewport"]');
 
     const viewportResize = () => {
-        const width = window.outerWidth > 360 ?
+        const width = window.outerWidth > 400 ?
             'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' :
             'width=360';
 
