21f74670a49f95da2be6176140242ecdc63f2960
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-01-17 16:08:53 +0900

    [Change] js-mouseFollow-Center  Changed the value of the transition property when hovering so that the string "CLICK" can be displayed. Added a hidden area for this purpose.
    - transition property
    Delayed processing of the center part by transform was done in style.css, so we improved it so that the style is specified in JS. overwriting by style is prevented.
    
    - Display the "CLICK" string.
    Expanded the hover area to "topMainvisual" in consideration of the future implementation of video modal that allows video playback.
    Along with that, header and newsticker hide the mouse stalker.
    
    - Added a style to hide the mouse stalker.

--- assets/dist/js/pointer.js
+++ assets/dist/js/pointer.js
@@ -2,17 +2,20 @@
 window.addEventListener('load', () => {
     new MouseFollow(992);
 });
-// const FOCUSABLE = 'a, area, input, button, select, option, textarea, output, summary, video, audio, object, embed, iframe';
-const FOCUSABLE = 'area, input, select, option, textarea, output, summary, video, audio, object, embed, iframe';
 
+/**
+ * 非表示にしたいセレクターや領域(class)を文字列で記述します。
+ */
+const FOCUSABLE = 'area, input, select, option, textarea, output, summary, video, audio, object, embed, iframe, .s-header, .c-newsticker';
 class MouseFollow {
     constructor(breakpoints) {
-        const o = {
+        this.o = {
             mouseCenter: 'js-mouseFollow-Center',
-            mouseHoverTargets: 'a:not(.no-focus-hover), button[type="button"]',
+            mouseHoverSelecter: 'a, button[type="button"]',
+            mouseHoverPlay: '.p-topMainvisual',
         };
-        this.mouseCenter = document.querySelector(`.${o.mouseCenter}`);
-        this.mouseHoverTargets = document.querySelectorAll(`${o.mouseHoverTargets}`);
+        this.mouseCenter = document.querySelector(`.${this.o.mouseCenter}`);
+        this.mouseHoverTargets = document.querySelectorAll(`${[this.o.mouseHoverSelecter, this.o.mouseHoverPlay]}`);
         this.unSelectable = document.querySelectorAll(FOCUSABLE);
         this.hoverbool = false;
         this.breakpoints = breakpoints;
@@ -30,6 +33,7 @@ class MouseFollow {
     movePointer() {
         document.addEventListener('mousemove', (e) => {
             this.mouseCenter.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+            this.mouseCenter.style.transition = `transform 0.3s ease-out 0s`;
         });
     }
 
@@ -38,7 +42,7 @@ class MouseFollow {
             const element = this.mouseHoverTargets[i];
             // hover
             element.addEventListener('mouseover', (e) => {
-                this.mouseCenter.classList.add('hover');
+                this.mouseCenter.classList.add('hover'); 
             });
             // not hover
             element.addEventListener('mouseout', (e) => {
