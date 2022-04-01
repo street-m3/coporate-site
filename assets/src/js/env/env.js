17878325f4b646fcfdbe8017e5bd6433844fb5ae
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-04-02 01:28:08 +0900

    [Change] no message

--- assets/src/js/env/env.js
+++ assets/src/js/env/env.js
@@ -2,7 +2,7 @@
 const globalvariables = {
     siteName: '藤岡行政書士事務所',
     // siteUrl: 'http://' + location.host + '/', //環境用
-    siteUrl: 'https://deploy-preview-44--reverent-volhard-d99d5d.netlify.app/', //本番用
+    siteUrl: 'https://reverent-volhard-d99d5d.netlify.app/', //本番用
     prefixid: '#index_id',
 };
 export { globalvariables };
