e368810c310e1cc42787b202ac1a44886939c5ce
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-04-10 13:55:24 +0900

    [Disable] Disable duplicate site URLs

--- assets/src/js/env/env.js
+++ assets/src/js/env/env.js
@@ -1,8 +1,23 @@
-
+'use strict';
 const globalvariables = {
     siteName: '藤岡行政書士事務所',
-    // siteUrl: 'http://' + location.host + '/', //環境用
+    developmentUrl: 'http://' + location.host + '/', //環境用
     siteUrl: 'https://reverent-volhard-d99d5d.netlify.app/', //本番用
     prefixid: '#index_id',
+    images: 'images/',
 };
-export { globalvariables };
+
+const parentIndexPath = {
+    index: 'ホーム',
+    service: 'サービス',
+    price: '価格',
+    news: 'お知らせ',
+    blog: 'ブログ',
+    company: '会社情報',
+    faq: 'よくある質問',
+    sitemap: 'サイトマップ',
+    policy: 'プライバシーポリシー',
+    contact: 'お問い合わせ',
+};
+
+export { globalvariables, parentIndexPath };
