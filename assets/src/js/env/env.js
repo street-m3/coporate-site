2d29a8c9f94b32450117c1d26df286d2bd91f714
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-04-24 20:34:26 +0900

    [Delete]  function
    Review environment variables and lexical scope of handler functions

--- assets/src/js/env/env.js
+++ assets/src/js/env/env.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const presets = {
     siteName: '藤岡行政書士事務所',
     developmentUrl: 'http://' + location.host + '/', //環境用
@@ -7,26 +8,4 @@ const presets = {
     images: 'images/',
 };
 
-const { REQUEST_KEY_BASE, XMICROCMS_API_KEY } = process.env;
-
-if (REQUEST_KEY_BASE === undefined || XMICROCMS_API_KEY === undefined) {
-    throw new Error("Could not retrieve environment variables. Please review your settings....(;_;)");
-}
-
-/**
- * { string } URL
- * @returns 
- */
-const requestURLconnected = () => {
-    if (typeof REQUEST_KEY_BASE === "string") return REQUEST_KEY_BASE;
-}
-
-/**
- * { string } API_KEY
- * @returns 
- */
-const requestAPIconnected = () => {
-    if (typeof XMICROCMS_API_KEY === "string") return XMICROCMS_API_KEY;
-}
-
-export { presets , requestURLconnected, requestAPIconnected};
+export { presets};
