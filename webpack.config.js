4ec438bb61dbfc4b9b465cafea5bd3535a6cf6fb
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-04-26 11:08:26 +0900

    [Delete] dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,6 @@
-const webpack = require('webpack');
-const dotenv = require('dotenv');
-
-const env = dotenv.config().parsed;
 module.exports = {
     mode: 'production', //production or development
     entry: `./assets/src/js/main.js`,
-    plugins: [
-        env !== undefined
-        ? new webpack.DefinePlugin({
-            "process.env": JSON.stringify(env),
-        })
-        : new webpack.DefinePlugin({
-            "process.env.REQUEST_KEY_BASE": JSON.stringify(process.env.REQUEST_KEY_BASE),
-            "process.env.XMICROCMS_API_KEY": JSON.stringify(process.env.XMICROCMS_API_KEY),
-        })
-    ],
     
     output: {
         path: `${__dirname}/assets/dist/js`,
